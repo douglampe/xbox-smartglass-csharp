ef2248540dfccc1fea5ef4e699389c80a75bbb14
Doug Lampe <rlampe@deloitte.com>
2020-10-23 14:41:24 -0400

    Add Dockerfile files for base and arm

---
+++ docker-build-arm.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -f Dockerfile.arm -t douglampe/xbox-smartglass-csharp-arm:local .
---
+++ docker-build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -f Dockerfile -t douglampe/xbox-smartglass-csharp:local .
